c92025484eb9efde79413170cac927459256ec8e
viswa08 <viswa0894@gmail.com>
2018-03-21 11:46:38 +0530

    initial commit

---
+++ struct_trial.ino
@@ -0,0 +1,41 @@
+
+ struct numbers
+ {
+  int a = 10, b = 20, c, d;
+ };
+
+ struct numbers mathop;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+
+
+
+
+int structureDemo();
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ structureDemo();
+ Serial.println(mathop.c);
+ Serial.println(mathop.d);
+ delay(2000); 
+ 
+ 
+}
+
+int structureDemo()
+{
+  int e = 50;
+  mathop.c = mathop.a +mathop.b + e;
+  mathop.d = mathop.b - mathop.a; 
+}
+
+
+
+
